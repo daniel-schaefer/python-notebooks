e6fd84c2e6da61af9f25a27cd0101d28b707ca1c
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-12 15:20:12 +0200

    Update 00.Installation.py

--- 00.Installation.py
+++ 00.Installation.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.1.1
+#       jupytext_version: 1.2.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3.7
 #     language: python
 #     name: python3
 # ---
@@ -21,6 +21,16 @@
 # ## With conda
 #
 # ### 1. Install [Anaconda](https://www.anaconda.com/downloads) (large) or [Miniconda](https://conda.io/miniconda.html) (small)
+#
+# Download the Python 3.7 64-bit (bash installer)
+#
+# In your terminal window, run:
+#
+#   * Miniconda:
+#
+# ```bash
+#        bash Miniconda3-latest-Linux-x86_64.sh
+# ```
 
 # %% [markdown] {"slideshow": {"slide_type": "slide"}}
 # ### 2. Open a terminal (Linux/MacOSX) or a conda prompt (Windows)
@@ -30,7 +40,7 @@
 #
 #
 # ```bash
-# conda create python=3.6 -n my-env
+# conda create python=3.7 -n math-python
 # ```
 
 # %% [markdown] {"slideshow": {"slide_type": "slide"}}
@@ -38,9 +48,9 @@
 #
 # Activating the conda environment will change your shell’s prompt to show what virtual environment you’re using, and modify the environment so that running python will get you that particular version and installation of Python. 
 # <pre>
-# $ conda activate my-env
+# $ conda activate math-python
 # (math-python) $ python
-# Python 3.6.2 (default, Jul 17 2017, 16:44:45) 
+# Python 3.7.4 (default, Jul 17 2019, 16:44:45) 
 # [GCC 4.2.1 Compatible Apple LLVM 8.1.0 (clang-802.0.42)] on darwin
 # Type "help", "copyright", "credits" or "license" for more information.
 # >>> quit()
