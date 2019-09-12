265a2486f12a67aeb0f3d51b48e6b55ec4e4e886
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-12 17:06:26 +0200

    Update 02.Strings.py

--- 02.Strings.py
+++ 02.Strings.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.1.1
+#       jupytext_version: 1.2.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3.7
 #     language: python
 #     name: python3
 # ---
